0be4d932fa6b4c15016ee3ba6ad65ddb917663b6
Gregory Reeves Jr <gregoryreevesjr@gmail.com>
2020-09-19 10:59:36 -0500

    Challenge completed

---
+++ MergedListChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MergedListChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
